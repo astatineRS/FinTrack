244254676ce875c5aea6fd90c8028b5bedf5b901
Rudransh Singh Tomar <trudranshsingh2003@gmail.com>
2023-04-02 14:40:40 +0530

    code base

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
 
 
         <div className="w-full h-10 fixed bottom-0 bg-new flex justify-center items-center text-white" style={{ background: " linear-gradient(to right, #5c258d, #4389a2)" }} >
-            <label className='text-white font-normal text-lg' ><a target='_blank' href=""> Checkout on GitHub</a></label>
+            <label className='text-white font-normal text-lg' ><a target='_blank' href="https://github.com/trudransh/FinTrack"> Checkout on GitHub</a></label>
         </div>
     );
 }
